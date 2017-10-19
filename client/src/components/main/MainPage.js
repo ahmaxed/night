531671ec4367dc7376e48b1a25caa68f9f27fba2
ahmadabdolsaheb <ahmad.abdolsaheb@gmail.com>
2017-10-19 14:25:50 +0300

    get call loaded in mainPage

--- client/src/components/main/MainPage.js
+++ client/src/components/main/MainPage.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { loadBars } from '../../actions/eventActions';
+import { connect } from 'react-redux';
 
 class MainPage extends React.Component {
+
   render(){
+    this.props.loadBars("chicago").then( res => console.log(res.data));
     return (
       <div className="jumbotron">
         <h1> Hi </h1>
@@ -10,4 +14,4 @@ class MainPage extends React.Component {
   }
 }
 
-export default MainPage;
+export default connect(null, {loadBars})(MainPage);
